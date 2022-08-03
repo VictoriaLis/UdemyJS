2c632d46d93e75803bf986ac1b668c42a40c5510
Victoria <victorialis001@gmail.com>
2022-08-03 21:19:33 +0400

    film homework

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
 "use strict";
 
-console.log(2+2*2 !== '6');
+const numberOfFilms = prompt("Сколько фильмов вы уже посмотрели?", "");
+
+const latestFilmViewed = prompt("Один из последних просмотренных фильмов?", "");
+const filmRating = prompt("На сколько оцените его?", "");
+
+let personalMovieDB = {
+    count: numberOfFilms,
+    movies: {
+        [latestFilmViewed]: filmRating
+    },
+    actors: {
+
+    },
+    genres: [],
+    privat: false
+}
+console.log(personalMovieDB);
+
