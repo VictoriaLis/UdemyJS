57614127bb3ac88d6880420e289b690e38933f21
Victoria <45670917+VictoriaLis@users.noreply.github.com>
2022-09-03 19:17:06 +0400

    Film task completed (#1)
    * init film task
    
    * feat: film task completed

--- script.js
+++ script.js
@@ -1,20 +1,54 @@
-"use strict";
-
-const numberOfFilms = prompt("Сколько фильмов вы уже посмотрели?", "");
-
-const latestFilmViewed = prompt("Один из последних просмотренных фильмов?", "");
-const filmRating = prompt("На сколько оцените его?", "");
-
-let personalMovieDB = {
-    count: numberOfFilms,
-    movies: {
-        [latestFilmViewed]: filmRating
-    },
-    actors: {
-
-    },
-    genres: [],
-    privat: false
+// При помощи цикла выведите числа от 5 до 10 в консоль. 5 и 10 включительно. 
+// Цикл можно использовать любой
+for (let i = 5; i < 11; i++) {
+    console.log(i);
 }
-console.log(personalMovieDB);
 
+// При помощи цикла for вывести числа от 20 до 10 в консоль. В обратном порядке (20, 19, 18...). 
+// Когда цикл дойдет до числа 13 - остановить весь цикл
+for (let i = 20; i >= 10; i--) {
+    if (i == 13) {
+        break;
+    }
+    console.log(i);
+}
+
+// При помощи цикла for выведите чётные числа от 2 до 10 включительно
+for (let i = 2; i < 11; i++) {
+    if (i % 2 != 0) {
+        continue;
+    }
+    console.log(i);
+}
+
+// Перепишите цикл  for на вариант с while. Результат должен остаться точно таким же. 
+// Не создайте бесконечный цикл! Иначе браузер может зависнуть.
+//  Цикл, который нужно переписать:
+
+// for (let i = 2; i <= 16; i++) {
+//     if (i % 2 === 0) {
+//         continue;
+//     } else {
+//         console.log(i);
+//     }
+// }
+
+let i = 2;
+while (i < 16) {
+    i++;
+    if (i % 2 === 0) {
+        continue;
+    } else {
+        console.log(i);
+    }
+}
+
+// Заполните массив цифрами от 5 до 10 включительно.
+// Элементы массива можно сформировать так же, как и обращаться к ним: arr[0]
+
+let ourArray = [];
+
+for (let i = 5; i <= 10; i++) {
+    ourArray[ourArray.length] = i;
+}
+console.log(ourArray);
