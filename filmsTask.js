0855c7e4a39a7471b901699b47ad53a650465511
Victoria <victorialis001@gmail.com>
2022-09-03 19:06:55 +0400

    feat: film task completed

--- filmsTask.js
+++ filmsTask.js
@@ -1,16 +1,9 @@
-let numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?');
-
-let latestFilm = prompt('Один из последних просмотренных фильмов?');
-let filmRate = prompt('На сколько оцените его?');
-let latestFilm1 = prompt('Один из последних просмотренных фильмов?');
-let filmRate1 = prompt('На сколько оцените его?');
-
+let numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', "");
 
 let personalMovieDB = {
     count: numberOfFilms,
     movies: {
-        [latestFilm]: filmRate,
-        [latestFilm1]: filmRate1
+
     },
     actors: {
 
@@ -19,3 +12,45 @@ let personalMovieDB = {
     privat: false
 };
 
+if (personalMovieDB.count < 10) {
+    alert("Просмотрено довольно мало фильмов");
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count <= 30) {
+    alert("Вы классический зритель");
+} else if (personalMovieDB.count > 30) {
+    alert("Вы киноман");
+} else {
+    alert("Произошла ошибка");
+}
+
+
+for (let i = 0; i < 2; i++) {
+    const latestFilm = prompt('Один из последних просмотренных фильмов?', "");
+    const filmRate = prompt('На сколько оцените его?', "");
+
+    if ((latestFilm == "" || filmRate == "") || (latestFilm == null || filmRate == null) || latestFilm.length > 50) {
+        i--;
+        continue;
+    }
+
+    personalMovieDB.movies[latestFilm] = filmRate;
+}
+console.log(personalMovieDB);
+
+// _____________________________________________
+let i = 0;
+
+while (i < 2) {
+    
+    const latestFilm = prompt('Один из последних просмотренных фильмов?', "");
+    const filmRate = prompt('На сколько оцените его?', "");
+
+    if ((latestFilm == "" || filmRate == "") || (latestFilm == null || filmRate == null) || latestFilm.length > 50) {
+        i--;
+        continue;
+    }
+
+    personalMovieDB.movies[latestFilm] = filmRate;
+
+    i++; 
+
+}
