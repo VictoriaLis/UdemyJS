7263ae910004bebec6e0c173363c3f42f996b78c
Victoria <45670917+VictoriaLis@users.noreply.github.com>
2023-01-21 18:35:01 +0300

    feat: 35 & 37 lessons completed (#4)

--- filmsTask.js
+++ filmsTask.js
@@ -9,48 +9,49 @@ let personalMovieDB = {
 
     },
     genres: [],
-    privat: false
+    privat: false,
+
+    showFilmActivity: function () {
+        if (this.count < 10) {
+            alert("Просмотрено довольно мало фильмов");
+        } else if (this.count >= 10 && this.count <= 30) {
+            alert("Вы классический зритель");
+        } else if (this.count > 30) {
+            alert("Вы киноман");
+        } else {
+            alert("Произошла ошибка");
+        }
+    },
+
+    setFilmRates: function () {
+        for (let i = 0; i < 2; i++) {
+            const latestFilm = prompt('Один из последних просмотренных фильмов?', "");
+            const filmRate = prompt('На сколько оцените его?', "");
+
+            if ((latestFilm === "" || filmRate === "") || (latestFilm === null || filmRate === null) || latestFilm.length > 50) {
+                i--;
+                continue;
+            }
+
+            this.movies[latestFilm] = filmRate;
+        }
+    },
+
+    toggleVisibleMyDB: function () {
+        this.privat = !this.privat;
+    },
+
+    writeYourGenres: function () {
+        const genres = [];
+        for (let i = 0; i < 3; i++) {
+            const genre = prompt("Любимый жанр?");
+            if (genre === "" || genre === null) {
+                i--;
+                continue;
+            }
+            genres.push(genre);
+        }
+
+        genres.forEach((element, index) => console.log(`Любимый жанр #${index+1} это ${element}`));
+    }
 };
-
-if (personalMovieDB.count < 10) {
-    alert("Просмотрено довольно мало фильмов");
-} else if (personalMovieDB.count >= 10 && personalMovieDB.count <= 30) {
-    alert("Вы классический зритель");
-} else if (personalMovieDB.count > 30) {
-    alert("Вы киноман");
-} else {
-    alert("Произошла ошибка");
-}
-
-
-for (let i = 0; i < 2; i++) {
-    const latestFilm = prompt('Один из последних просмотренных фильмов?', "");
-    const filmRate = prompt('На сколько оцените его?', "");
-
-    if ((latestFilm == "" || filmRate == "") || (latestFilm == null || filmRate == null) || latestFilm.length > 50) {
-        i--;
-        continue;
-    }
-
-    personalMovieDB.movies[latestFilm] = filmRate;
-}
-console.log(personalMovieDB);
-
-// _____________________________________________
-let i = 0;
-
-while (i < 2) {
-    
-    const latestFilm = prompt('Один из последних просмотренных фильмов?', "");
-    const filmRate = prompt('На сколько оцените его?', "");
-
-    if ((latestFilm == "" || filmRate == "") || (latestFilm == null || filmRate == null) || latestFilm.length > 50) {
-        i--;
-        continue;
-    }
-
-    personalMovieDB.movies[latestFilm] = filmRate;
-
-    i++; 
-
-}
